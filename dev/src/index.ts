969ab9a492b8549c2d059ac4e73569c45fd1b872
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-07-17 17:57:59 -0300

    Context test

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -1,3 +1,31 @@
 console.log("WOAH!");
 
+interface ContextMenuEntry {
+    classes?: string;
+    condition?: (li: JQuery<HTMLElement>) => boolean;
+    callback: (li: JQuery<HTMLElement>) => void;
+    group?: string;
+    icon?: string;
+    name: string;
+}
+
+foundry.helpers.Hooks.on("getCompendiumContextOptions", (
+	app: any, 
+	html: any) => { 
+
+	console.log(app); console.log(html);
+	console.log("CONTEXT")
+}
+);
+
+foundry.helpers.Hooks.on("renderCompendiumDirectory", (
+	app: any, 
+	html: any) => { 
+
+	console.log(app); console.log(html);
+	console.log("RENDER")
+}
+);
+
+
 console.log(Hooks)
