3e14cccf267369ceb68eda4f798b4fdf5e5407a1
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-07-17 18:01:10 -0300

    Applied test version.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 "use strict";
 console.log("WOAH!");
+foundry.helpers.Hooks.on("getCompendiumContextOptions", (app, html) => {
+    console.log(app);
+    console.log(html);
+    console.log("CONTEXT");
+});
+foundry.helpers.Hooks.on("renderCompendiumDirectory", (app, html) => {
+    console.log(app);
+    console.log(html);
+    console.log("RENDER");
+});
 console.log(Hooks);
